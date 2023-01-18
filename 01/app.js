1dbc6857875aa2ffcfa3963c4c174461c21c846f
rinsarmu <roberaensermu@gmail.com>
2023-01-18 11:34:37 +0300

    the first project

--- 01/app.js
+++ 01/app.js
@@ -1,5 +1,7 @@
 const fs = require('fs')
 const http = require('http')
+const path = require('path')
+const httpUrl = require('url')
 
 const port = '8000'
 const host = '127.0.0.1'
@@ -8,7 +10,7 @@ const data = fs.readFileSync(`${__dirname}/Data/packData.json`)
 const dataObj = JSON.parse(data)
 
 const overview = fs.readFileSync(`${__dirname}/template/template__overview.html`, 'utf-8')
-const product = fs.readFileSync(`${__dirname}/template/template__product.html`, 'utf-8')
+const productTemplate = fs.readFileSync(`${__dirname}/template/template__product.html`, 'utf-8')
 
 const card = fs.readFileSync(`${__dirname}/template/card.html`, 'utf-8')
 
@@ -36,10 +38,12 @@ const replaceTemplate = (temp, element) =>{
 const server = http.createServer((req, res)=>{
     const url = req.url;
 
+    const {query, pathname} = httpUrl.parse(req.url, true)
+console.log(query, pathname);
     //Routing
 
     //overview
-    if(url === '/overview' || url === '/'){
+    if(pathname === '/overview' || pathname === '/'){
         res.writeHead(200, {
             'Content-Type': 'text/html'
         })
@@ -50,14 +54,20 @@ const server = http.createServer((req, res)=>{
     } 
 
     //product
-    else if(url==='/product'){
+    else if(pathname==='/product'){
+        console.log(query);
         res.writeHead(200, {
             'Content-Type': 'text/html'
         })
+
+        const product = dataObj[query.id]
+        console.log(product);
+        const output = replaceTemplate(productTemplate, product)
+        res.end(output)
     } 
 
     //api
-    else if(url ==='/api') {
+    else if(pathname ==='/api') {
         fs.readFile(`${__dirname}/Data/packData.json`, 'utf-8', (err, data) =>{
             if(!err) {
                 return res.end(data)
