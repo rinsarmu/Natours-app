b5006c52db1c29fc5a186b586eb3402784960b76
rinsarmu <roberaensermu@gmail.com>
2023-01-24 18:49:18 +0300

    Creating streams of Data

--- Event-LOOP/Streams/streams.js
+++ Event-LOOP/Streams/streams.js
@@ -2,7 +2,12 @@ const fs = require('fs')
 const server = require('http').createServer()
 
 server.on('request', (req, res) =>{
-    console.log("Requesting");
+
+    const readable = fs.createReadStream('../text.txt')
+    readable.on('data', chunk=>{
+        res.end(chunk)
+    })
+
 })
 
 server.listen(8000, '127.0.0.1', ()=>{
